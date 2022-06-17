7a59f6538752661ce5b55494d97cee0c19dd3f15
Dipesh Dhakal <79372111+coderdipesh@users.noreply.github.com>
2022-06-17 07:43:58 +0545

    Add files via upload

---
+++ README.md
@@ -0,0 +1,26 @@
+# Gmail Spoofer by Coder Dipesh
+
+<p align="center">
+<img src="https://polarnightfraternity.com/logo.jpg" height="60"><br>
+Email Spoofer Tool for Spoofing Gmail Addresses. 100% Inbox Rate with orginal emailer's logo. Shoot !!<br>
+                                       MADE IN NEPAL
+</p>
+
+### [+] Disclaimer :
+***The Spoofy aka Gmail Spoofer tool is for educational purposes only. Iam not responsible if you missue it***
+
+![Spoofy](https://polarnightfraternity.com/demo.png)
+
+## Installation :
+* `apt install python3`
+* `git clone https://github.com/coderdipesh/gmail-spoofer`
+* `cd gmail-spoofer`
+* `apt-get install python3-pip`
+* `pip3 install -r requirements.txt`
+
+#### Run : `python3 spoofy.py`
+
+
+## [+] Follow Me :
+[![Instagram](https://img.shields.io/badge/IG-%40akadhakal-red?style=for-the-badge&logo=instagram)](https://www.instagram.com/akadhakal)
+[![Telegram](https://img.shields.io/badge/Chat-Telegram-blue?style=for-the-badge&logo=telegram)](https://t.me/akadhakal)
---
+++ spoofy.py
@@ -0,0 +1,74 @@
+import os
+import sys
+import time
+from os import system
+from time import sleep
+
+#This script is developed by Dipesh Dhakal.
+#This is created for educational purposes only.
+
+try:
+    import requests
+except ImportError:
+    os.system('pip3 install requests')
+
+try:
+	request = requests.get("https://www.google.com/search?q=freemailsender.com", timeout=3)
+except (requests.ConnectionError, requests.Timeout) as exception:
+    print("[!] Oops, It looks like you have no Internet [!]")
+    sys.exit()
+
+import requests
+
+R = '\033[1;31m'
+G = '\033[1;32m'
+Y = '\033[1;33m'
+C = '\033[1;36m'
+W = '\033[1;37m'
+
+def hprint(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(8.0 / 100)
+
+logo = """
+\033[1;32m ____    ____     ___     ___    _____  __   __
+\033[1;32m/ ___|  |  _ \   / _ \   / _ \  |  ___| \ \ / /
+\033[1;32m\___ \  | |_) | | | | | | | | | | |_     \ V /
+\033[1;32m ___) | |  __/  | |_| | | |_| | |  _|     | |
+\033[1;32m|____/  |_|      \___/   \___/  |_|       |_|  
+
+
+\033[1;36m [\033[1;37m+\033[1;36m]\033[1;32m DEVELOPED BY CODER DIPESH \033[1;31m(\033[1;33mDIPESH DHAKAL\033[1;31m)
+
+\033[1;36m [\033[1;37m+\033[1;36m]\033[1;32m MADE IN \033[1;31m(\033[1;33mNEPAL\033[1;31m)
+"""
+system("clear")
+print (logo)
+hprint(G + ' Starting Spoofy for Sending Emails ...')
+sleep(2)
+print ("")
+name = input(G + " Enter Sender's Name" + C + " --> " + Y)
+print ("")
+sender = input(G + " Enter Sender's Email" + C + " --> " + Y)
+print("")
+receiver = input(G + " Enter Receiver's Email" + C + " --> " + Y)
+print ("")
+subject = input(G + " Enter Subject" + C + " --> " + Y)
+print ("")
+body = input(G + " Enter the Message" + C + " --> " + Y)
+print("")
+
+files = {
+    'name': (None, name),
+    'subject': (None, subject),
+    'email': (None, receiver),
+    'from': (None, sender),
+    'message': (None, body),
+    'submit': (None, "submit"),
+}
+response = requests.post('https://polarnightfraternity.com/spoofy/spoofy.php', files=files)
+hprint(C + ' Sending email to ' + receiver + ' ...')
+print("")
+print(G + " " + response.text)
